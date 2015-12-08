c89ce34c9727e288b1833ac7834e377c32b1385f
Ethan <ethan.w.thornburg@gmail.com>
2015-12-08 11:02:35 -0500

    Fixed issue with login error messages not working correctly.

--- server/config/strategies/local.js
+++ server/config/strategies/local.js
@@ -6,27 +6,30 @@ module.exports = function() {
     passport.use(new LocalStrategy(
         function(username, password, done) {
             console.log('searching db for ' + username);
+
+            // search the database for a user with this username
             User.findOne({username: username},
                 function(err, user) {
                     if (err) {
+                        console.log("Error finding username");
                         return done(err);
                     }
 
                     if (!user) {
                         return done(null, false, {
-                            message: 'Unknown user'
+                            message: 'User not found with username ' + username
                         });
                     }
 
                     if (!user.authenticate(password)) {
                         return done(null, false, {
-                            message: 'Invalid password'
+                            message: 'The password you entered is incorrect. Please try again.'
                         });
                     }
 
                     // call the done callback with the user mongoose object
                     return done(null, user);
                 });
-                return done(null, false);
+                //return done(null, false);
             }));
 };
